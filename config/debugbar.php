5237781126fcf1c1419b3b2b60e09586bb6d3c41
Marcus T <marcust261@icloud.com>
2016-05-26 13:13:20 +0100

    Style fixes

--- config/debugbar.php
+++ config/debugbar.php
@@ -135,7 +135,7 @@ return [
             'data' => false,    //Note: Can slow down the application, because the data can be quite large..
         ],
         'route' => [
-            'label' => true , // show complete route on bar
+            'label' => true, // show complete route on bar
         ],
         'logs' => [
             'file' => null,
